c210fb236e771b4e69ac10392fdac737c1cd5dbe
Mounish Soni <mounishsoni116@gmail.com>
2025-03-29 18:10:02 -0400

    added card grid

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,21 +5,12 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
 	<main>
 		<!-- <TheWelcome /> -->
-		<div>
+		<div style="margin:auto 20px;">
 			<br />
 			<Toolbar>
 				<template #start>
-					<Button
-						icon="pi pi-plus"
-						class="mr-2"
-						severity="secondary"
-					/>
-					<Button
-						icon="pi pi-refresh"
-						@click="fetchData"
-						class="mr-2"
-						severity="secondary"
-					/>
+					<Button icon="pi pi-plus" class="mr-2" severity="secondary" />
+					<Button icon="pi pi-refresh" @click="fetchData" class="mr-2" severity="secondary" />
 				</template>
 
 				<template #center>
@@ -27,55 +18,43 @@ import TheWelcome from '../components/TheWelcome.vue'
 						<InputIcon>
 							<i class="pi pi-search" />
 						</InputIcon>
-						<InputText
-							placeholder="Search"
-							:value="collectionSF"
-							@value-change="setCollectionSF"
-						/>
+						<InputText placeholder="Search" :value="collectionSF" @value-change="setCollectionSF" />
 					</IconField>
 				</template>
 
 				<template #end>
-					<SplitButton label="Save" :model="items"></SplitButton
-				></template>
+					<SplitButton label="Save" :model="items"></SplitButton>
+				</template>
 			</Toolbar>
 			<br />
-			<div v-for="item in filteredCollectionData" :key="item.id">
-				<Card style="width: 25rem; overflow: hidden">
-					<template #header>
-						<!-- <img alt="user header" src="/images/usercard.png" /> -->
-					</template>
-					<template #title>{{ item.title }}</template>
-					<template #subtitle>{{ item.description }}</template>
-					<template #content>
-						<p class="m-0">
-							Explore all Shiv Yog Forum Episodes. Click Learn
-							more to learn about what is Shiv Yog?
-						</p>
-					</template>
-					<template #footer>
-						<div class="flex gap-4 mt-1">
-							<Button
-								label="Learn more"
-								severity="secondary"
-								outlined
-								class="w-full"
-								@click="goToLink()"
-							/>
-							<Button
-								label="Episodes"
-								class="w-full"
-								@click="goToNextPage(item.id)"
-							/>
-						</div>
-					</template>
-				</Card>
+			<div class="grid">
+				<div class="col-12 md:col-6 lg:col-4" v-for="item in filteredCollectionData" :key="item.id">
+					<Card>
+						<template #header>
+							<!-- <img alt="user header" src="/images/usercard.png" /> -->
+						</template>
+						<template #title>{{ item.title }}</template>
+						<template #subtitle>
+							<Chip class="text-xs px-2 py-1" :label="`${item.sub_title}`" v-if="item.sub_title" />
+						</template>
+						<template #content>
+							{{ item.description }}
+						</template>
+						<template #footer>
+							<div class="flex gap-4 mt-1">
+								<Button label="Learn more" severity="secondary" outlined class="w-full"
+									@click="goToLink()" />
+								<Button label="Episodes" class="w-full" @click="goToNextPage(item.id)" />
+							</div>
+						</template>
+					</Card>
+				</div>
 			</div>
 		</div>
 	</main>
 </template>
 
-    <script>
+<script>
 import { supabase } from "@/config/supabaseClient";
 import { ref, watchEffect } from "vue";
 import { useRouter } from "vue-router";
@@ -115,7 +94,8 @@ export default {
 		async fetchData() {
 			const { data, error } = await supabase
 				.from("collection")
-				.select("*");
+				.select("*")
+				.order("title", { ascending: true });
 
 			if (error) {
 				console.error("Error fetching data:", error);
@@ -140,8 +120,8 @@ export default {
 			});
 		},
 	},
-    mounted() {
-        this.fetchData();
-    },
+	mounted() {
+		this.fetchData();
+	},
 };
 </script>
